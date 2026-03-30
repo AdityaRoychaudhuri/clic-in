1256d19de8bce5a5173fdc96dbf9176d7c76834f
Aditya <adiroychaudhuri@gmail.com>
2026-03-30 23:39:10 +0530

    Added the theme toggle component.

---
+++ components/ThemeToggle.jsx
@@ -0,0 +1,43 @@
+"use client"
+import React,{ useEffect, useState } from 'react'
+import { useTheme } from 'next-themes'
+import { Button } from './ui/button';
+import { Moon, Sun } from 'lucide-react';
+import { cn } from '@/lib/utils';
+
+const ThemeToggle = ({ className, iconSize = 20 }) => {
+    const { theme, setTheme } = useTheme();
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => setMounted(true), []);
+
+    if (!mounted) return null;
+
+  return (
+    <Button
+        variant='outline'
+        size='icon'
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        className={cn("relative rounded-full p-1 w-10 h-10 flex items-center justify-center mx-4", className)}
+    >
+        <Sun
+            className={cn(
+                "absolute transition-all duration-500 transform ease-[cubic-bezier(0.4,0,0.2,1)] size-5",
+                theme === "dark"
+                    ? "opacity-0 scale-75 -rotate-45"
+                    : "opacity-100 scale-105 rotate-0"
+            )}
+        />
+        <Moon
+            className={cn(
+                "absolute transition-all duration-500 transform ease-[cubic-bezier(0.4,0,0.2,1)] size-5",
+                theme === "dark"
+                    ? "opacity-100 scale-100 rotate-0"
+                    : "opacity-0 scale-75 rotate-45"
+            )}
+        />
+    </Button>
+  )
+}
+
+export default ThemeToggle
