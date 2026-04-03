7dade95fb0fbec7761e4e3b90f34936e635c341b
Aditya <adiroychaudhuri@gmail.com>
2026-04-03 23:29:48 +0530

    Created server actions for admin role

---
+++ actions/admin.js
@@ -0,0 +1,144 @@
+"use server"
+
+import { db } from "@/lib/prisma";
+import { VerificationStatus } from "@/prisma/generated/enums";
+import { auth } from "@clerk/nextjs/server"
+import { revalidatePath } from "next/cache";
+
+export async function verifyAdmin() {
+    const { userId } = await auth();
+
+    if (!userId) {
+        console.error("User is not authenticated");
+        return false;
+    }
+
+    try {
+        const user = await db.user.findUnique({
+            where: {
+                clerkId: userId
+            }
+        })
+
+        return user?.role === "ADMIN";
+    } catch (error) {
+        console.error("User not registered as admin!", error.message);
+        return false;
+    }
+}
+
+export async function getAllPendingDoctors() {
+    const isAdmin = await verifyAdmin();
+    if (!isAdmin) {
+        throw new Error("Unauthorised");
+    }
+
+    try {
+        const pendingDoctors = await db.user.findMany({
+            where: {
+                role: "DOCTOR",
+                verificationStatus: VerificationStatus.PENDING
+            },
+            orderBy: {
+                createdAt: "desc"
+            }
+        })
+
+        return {
+            doctors: pendingDoctors
+        }
+    } catch (error) {
+        console.error("Cannot perform this server action: ", error.message);
+        throw new Error("Cannot get pending doctor list");
+    }
+}
+
+export async function getAllVerifiedDoctors() {
+    const isAdmin = await verifyAdmin();
+    if (!isAdmin) {
+        throw new Error("Unauthorised");
+    }
+
+    try {
+        const pendingDoctors = await db.user.findMany({
+            where: {
+                role: "DOCTOR",
+                verificationStatus: VerificationStatus.VERIFIED
+            },
+            orderBy: {
+                createdAt: "asc"
+            }
+        })
+
+        return {
+            doctors: pendingDoctors
+        }
+    } catch (error) {
+        console.error("Cannot perform this server action: ", error.message);
+        throw new Error("Cannot get verified doctor list");
+    }
+}
+
+export async function verifyDoctorStatus(formData) {
+    const isAdmin = await verifyAdmin();
+    if (!isAdmin) {
+        throw new Error("Unauthorised");
+    }
+
+    const doctorId = formData.get("doctorId");
+    const status = formData.get("status");
+
+    if (!formData || !["VERIFIED", "REJECTED"].includes(status)) {
+        throw new Error("Invalid input");
+    }
+
+    try {
+        await db.user.update({
+            where: {
+                id: doctorId
+            },
+            data: {
+                verificationStatus: status
+            }
+        });
+
+        revalidatePath("/admin");
+        return {
+            success: true
+        }
+    } catch (error) {
+        console.error("Cannot perform this server action: ", error.message);
+        throw new Error("Cannot updated doctor status to database");
+    }
+}
+
+export async function updateDoctorStatus(formData) {
+    const isAdmin = await verifyAdmin();
+    if (!isAdmin) {
+        throw new Error("Unauthorised");
+    }
+
+    const doctorId = formData.get("doctorId");
+    const isSuspend = formData.get("status") === "suspend";
+
+    try {
+        const status = isSuspend ? "PENDING" : "VERIFIED";
+
+        await db.user.update({
+            where: {
+                id: doctorId,
+            },
+            data: {
+                verificationStatus: status
+            }
+        });
+
+        revalidatePath("/admin");
+        return {
+            success: true
+        }
+    } catch (error) {
+        console.error("Cannot perform this server action: ", error.message);
+        throw new Error("Cannot updated doctor status to database");
+    }
+}
