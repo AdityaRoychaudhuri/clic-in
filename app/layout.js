c5925872deab0326f5534de060950f4efa6d2b3a
Aditya <adiroychaudhuri@gmail.com>
2026-04-03 23:31:31 +0530

    added the toaster component for a better ui alerts for the application

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/Header";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import { Toaster } from "sonner";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -53,6 +54,7 @@ export default function RootLayout({ children }) {
             <main className="min-h-screen">
               {children}
             </main>
+            <Toaster richColors/>
             {/* Footer */}
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
