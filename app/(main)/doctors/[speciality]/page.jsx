dc240ea80542202a914d0bbd254d5de439101de7
Aditya <adiroychaudhuri@gmail.com>
2026-04-04 23:52:13 +0530

    init component for showing each speciality page and doctor information individually

--- app/(main)/doctors/[speciality]/page.jsx
+++ app/(main)/doctors/[speciality]/page.jsx
@@ -1,12 +1,35 @@
 "use client"
+import { getDoctorsBySpeciality } from '@/actions/doctorsList';
+import PageHeader from '@/components/PageHeader';
 import { useParams } from 'next/navigation'
 import React from 'react'
 
-const page = () => {
+const page = async () => {
     const {speciality} = useParams();
+    const specialityName = speciality.split("%20").join(" ");
+
+    const { doctors } = await getDoctorsBySpeciality(specialityName);
+
+
   return (
     <div>
-        Speciality page :  {speciality}
+        <PageHeader title={specialityName} backLink='/doctors' backToPageLabel='All specialities'/>
+
+        {doctors && doctors.length > 0 ? (
+          <div>
+            
+          </div>
+        ) : (
+          <div>
+            <h3>
+
+            </h3>
+
+            <p>
+
+            </p>
+          </div>
+        )}
     </div>
   )
 }
