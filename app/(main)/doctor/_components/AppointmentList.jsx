a07df0ceddd3be79932a83815fcb969e451fdc0c
Aditya <adiroychaudhuri@gmail.com>
2026-04-10 23:56:00 +0530

    created an individual doctor list component for the doctor page.

---
+++ app/(main)/doctor/_components/AppointmentList.jsx
@@ -0,0 +1,39 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Alert, AlertDescription } from '@/components/ui/alert'
+import { Calendar, AlertCircle } from 'lucide-react'
+import React from 'react'
+import AppointmentCard from '@/components/AppointmentCard'
+
+const AppointmentList = ({ appointmentList }) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className='flex gap-2 text-xl dark:text-white text-black font-bold items-center'>
+          <Calendar className='size-5 text-green-800 dark:text-green-600'/>
+          Upcoming Assignments
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        {appointmentList.length > 0 ? (
+          <div className='space-y-4'>
+            {appointmentList.map((appointment) => (
+              <AppointmentCard appointments={appointment} userRole={"doctor"}/>
+            ))}
+          </div>
+        ) : (
+          <div className=''>
+            <Alert className='text-center text-muted-foreground'>
+              <AlertDescription className='flex justify-center items-center gap-1 py-8 text-sm'>
+                <AlertCircle className='size-4'/>
+                You don&apos;t have any scheduled appointments yet. Make sure you have set your availability
+                to allow patients to book.
+              </AlertDescription>
+            </Alert>
+          </div>
+        )}
+      </CardContent>
+    </Card>
+  )
+}
+
+export default AppointmentList
