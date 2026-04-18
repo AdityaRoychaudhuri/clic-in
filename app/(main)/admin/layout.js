4054ca819aaa01e64f986bf5937bc5b5246e2759
Aditya <adiroychaudhuri@gmail.com>
2026-04-18 23:18:23 +0530

    minor UI changes inTabList and TabsTrigger

--- app/(main)/admin/layout.js
+++ app/(main)/admin/layout.js
@@ -20,7 +20,7 @@ const layout = async ({ children }) => {
         orientation='vertical'
       >
         <TabsList
-            className='md:col-span-1 h-14 md:h-28 w-full rounded-md bg-muted/45 flex sm:flex-row md:flex-col p-2 sm:p-1 space-y-2 md:space-y-1 sm:space-x-2 md:space-x-0'
+            className='md:col-span-1 h-14 md:h-fit w-full rounded-md bg-muted/45 flex sm:flex-row md:flex-col p-2 sm:p-1 space-y-2 md:space-y-1 sm:space-x-2 md:space-x-0'
         >
             <TabsTrigger 
                 value="pending"
@@ -37,11 +37,11 @@ const layout = async ({ children }) => {
                 <span>Doctors</span>
             </TabsTrigger>
             <TabsTrigger 
-                value="doctors"
+                value="payout"
                 className='p-2 flex-1 md:flex md:items-center md:justify-start md:px-4 md:py-3 w-full'
             >
                 <Users2 className='size-4 hidden md:inline'/>
-                <span>Doctors</span>
+                <span>Payout request</span>
             </TabsTrigger>
         </TabsList>
         <div className='md:col-span-3'>
